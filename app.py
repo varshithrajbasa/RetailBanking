3aa42fc9f03a72ee8db8274450de4b130c711339
arulpodduturi <40577716+arulpodduturi@users.noreply.github.com>
2020-06-15 19:37:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,23 +32,17 @@ def create_customer():
     message=""
     if request.method == 'POST':
         ssn_id=int(request.form.get('ssn-id'))
-        print(ssn_id)
         cust_id=int(request.form.get('cust-id'))
-        print(cust_id)
         cname=request.form.get('cname')
-        print(cname)
         c_age=int(request.form.get('cage'))
-        print(c_age)
         address=request.form.get('address')
-        print(address)
         city=request.form.get('secondlist')
-        print(city)
         state=request.form.get('listBox')
-        print(state)
-        curs = mysql.connection.cursor()
-        curs.execute('''INSERT INTO customer(ws_ssn, ws_cust_id, ws_name, ws_age, ws_adrs, city, state) values(%s, %s, %s, %s, %s, %s, %s)''',(ssn_id,cust_id,cname,c_age,address,city,state))
-        results = curs.fetchall()
-        if results:
+        if(mysql):
+            cursor= mysql.connection.cursor()
+            #cursor.execute("select * from customer")
+            cursor.execute('''INSERT INTO customer(ws_ssn, ws_cust_id, ws_name, ws_age, ws_adrs, city, state) values(%s, %s, %s, %s, %s, %s, %s)''',(ssn_id,cust_id,cname,c_age,address,city,state))
+            mysql.connection.commit()
             message="Done"
         else:
             message = "Error"
