9a8e99d3f8586d881c2b5463dec157e7fa24a378
arulpodduturi <40577716+arulpodduturi@users.noreply.github.com>
2020-06-18 10:50:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -227,6 +227,50 @@ def deposit():
     cur.execute('''INSERT INTO `retailbanking`.`transactions` (`ws_cust_id` ,`ws_accnt_type` ,`ws_amt` ,`ws_trxn_date` ,`ws_src_typ` ,`ws_tgt_typ`)VALUES (%s, %s, %s, %s , %s , %s);''',(id,a_type,balance,today,a_type,a_type))
     mysql.connection.commit()
     return render_template('deposit_money.html',user=userDetails,message=message)
+#for transfer
+@app.route("/transfer", methods=['post', 'get'])
+def transfer():
+    if session=={}:
+        return redirect(url_for('login'))
+    return render_template("transfer_money.html")
+@app.route("/transfer_amount", methods=['post', 'get'])
+def transfer_amount():
+    if session=={}:
+        return redirect(url_for('login'))
+    cust_id=int(request.form.get('id'))
+    print(cust_id)
+    tran_id=int(request.form.get('transfer_id'))
+    amount=int(request.form.get('transfer'))
+    cur = mysql.connection.cursor()
+    result=cur.execute('''select * from account where ws_cust_id= %s''',(cust_id,))
+    cust=cur.fetchall()
+    result1=cur.execute('''select * from account where ws_cust_id= %s''',(tran_id,))
+    trans=cur.fetchall()
+    today = date.today()
+    if result > 0:
+        if result1 > 0:
+            cust_after=cust[0][3]-amount
+            print(trans)
+            trans_after=trans[0][3]+amount
+            cur.execute('''update account set ws_acct_balance=%s where ws_cust_id=%s''',(cust_after,cust_id,))
+            mysql.connection.commit()
+            cur.execute('''update account set ws_acct_balance=%s where ws_cust_id=%s''',(trans_after,tran_id,))
+            mysql.connection.commit()
+            type1=cust[0][2]
+            cur.execute('''INSERT INTO `retailbanking`.`transactions` (`ws_cust_id` ,`ws_accnt_type` ,`ws_amt` ,`ws_trxn_date` ,`ws_src_typ` ,`ws_tgt_typ`)VALUES (%s, %s, %s, %s , %s , %s);''',(cust_id,type1,amount,today,type1,type1))
+            mysql.connection.commit()
+            type2=trans[0][2]
+            cur.execute('''INSERT INTO `retailbanking`.`transactions` (`ws_cust_id` ,`ws_accnt_type` ,`ws_amt` ,`ws_trxn_date` ,`ws_src_typ` ,`ws_tgt_typ`)VALUES (%s, %s, %s, %s , %s , %s);''',(tran_id,type2,amount,today,type2,type2))
+            mysql.connection.commit()
+            message="Amount transfer completed successfully"
+            return render_template('transfer_Details.html',message=message,cust=cust,trans=trans,cust_after=cust_after,trans_after=trans_after)
+
+        else:
+            message="Enter correct Target ID"
+    else:
+        message="Enter correct Customer ID"
+    return render_template("transfer_money.html",message=message)
+#for logout
 @app.route('/logout',methods=['post','get'])
 def logout():
     if session=={}:
